074f5d06ae1e4315a4d5a3aee8f2c3919ceac800
Anas IDRISSI <anasidrissi19@gmail.com>
2025-06-09 23:13:18 +0100

    update to use typescript, add stepSelector component for simple test

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import StepSelector from './components/StepSelector';
+
+function App() {
+  return (
+    <div className="bg-black min-h-screen text-white py-8">
+      <StepSelector />
+      <h1 className="text-3xl text-center">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          blue: '#0037C1',
+          darker: '#002a99' // 25% darker than primary-blue
+        },
+        secondary: {
+          blue: '#2563eb'
+        },
+        dark: {
+          bg: '#1C1C1C',
+          border: '#2A2A2A'
+        }
+      },
+    },
   },
   plugins: [],
 }
